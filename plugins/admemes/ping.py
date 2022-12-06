d6491cf3a95d32a480d0d54fd271ab13c8c9c9a3
GoodLove1 <110181533+GoodLove1@users.noreply.github.com>
2022-12-06 19:16:50 +0530

    Add files via upload

---
+++ plugins/admemes/ping.py
@@ -0,0 +1,49 @@
+"""Telegram Ping / Pong Speed
+Syntax: .ping"""
+
+import time
+import random
+from pyrogram import Client, filters
+from info import COMMAND_HAND_LER
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+
+# -- Constants -- #
+ALIVE = "He's not dead, honey, he's here. You don't have any love for me anymore. Oh great.. You're not the same as wast. 😔 At least occasionally, just try one/start. 🙂 " 
+REPO = "<b>𝙳𝙴𝙿𝙻𝙾𝚈 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 ›› https://www.youtube.com/shorts/v3TtNplrek4</b>"
+CHANNEL = "<b>𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻</b> ›› https://www.youtube.com/shorts/v3TtNplrek4\n\n<b>𝚄𝙿𝙳𝙰𝚃𝙴𝚂 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/fondness_movi</b>\n\n<b>𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/source_code_fondness</b>"
+ANURAG = "<b>𝙱𝙾𝚃 ›› https://t.me/BetterFilters_Ro_Bot</b>"
+# -- Constants End -- #
+
+
+@Client.on_message(filters.command("alive", COMMAND_HAND_LER) & f_onw_fliter)
+async def check_alive(_, message):
+    await message.reply_text(ALIVE)
+
+
+@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
+
+
+@Client.on_message(filters.command("repo", COMMAND_HAND_LER) & f_onw_fliter)
+async def repo(_, message):
+    await message.reply_text(REPO)
+
+
+@Client.on_message(filters.command("group", COMMAND_HAND_LER) & f_onw_fliter)
+async def group(_, message):
+    await message.reply_text(GROUP)
+
+
+@Client.on_message(filters.command("channel", COMMAND_HAND_LER) & f_onw_fliter)
+async def channel(_, message):
+    await message.reply_text(CHANNEL)
+
+
+@Client.on_message(filters.command("anurag", COMMAND_HAND_LER) & f_onw_fliter)
+async def ajax(_, message):
+    await message.reply_text(ANURAG)
