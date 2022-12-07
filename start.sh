95fb8fe7fec007701904db49515e83f7e6a5efe4
GoodLove1 <110181533+GoodLove1@users.noreply.github.com>
2022-12-07 20:20:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GoodLove1/Anurag-Extra-Features.git /Anurag-Extra-Features
+  git clone https://github.com/GoodLove1/fondness.git /Anurag-Extra-Features
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Anurag-Extra-Features
