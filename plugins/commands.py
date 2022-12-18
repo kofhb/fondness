f29c707eff197322de36fe07cab9552f38b9bc37
GoodLove1 <110181533+GoodLove1@users.noreply.github.com>
2022-12-18 14:01:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -253,7 +253,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/channel/UCqts9WhhlioK3RB9XQQzoAg') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://www.youtube.com/shorts/v3TtNplrek4') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
