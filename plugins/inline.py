6cca25d66e6727086d3fea8624c82e2a98f8fd65
GoodLove1 <110181533+GoodLove1@users.noreply.github.com>
2022-12-22 07:28:52 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -43,7 +43,7 @@ async def answer(bot, query):
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format( {file_name=title},{file_size=size} ,{duration})
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
 
 
 
