1fc68c223e26c18fb84745aae3a0c31855c41109
GoodLove1 <110181533+GoodLove1@users.noreply.github.com>
2022-12-24 11:40:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,12 +39,12 @@ class evamaria(Client):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID',"12635510"))
 API_HASH = environ.get('API_HASH',"da59e4e56ec4fe35af603bd30208ecc5")
-BOT_TOKEN = environ.get('BOT_TOKEN',"5840228189:AAFdcjx5rWTJk8SGUahRtXEVq-4tT_xFGT8")
+BOT_TOKEN = environ.get('BOT_TOKEN',"")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,"https://telegra.ph/file/3445aaf7bdd09bb83bdf0.jpg")).split()
+PICS = (environ.get('PICS' ,"")).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS',"1130215726").split()]
